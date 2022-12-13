20e6335be1458320ff826a33fc38d0ad19dca8e2
OscarwOw <OscarwOw210@gmail.com>
2022-12-13 21:40:48 +0100

    Add project files.

---
+++ Data/DatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityAppCourse2022.Data
+{
+    public class DatabaseContext : IdentityDbContext<IdentityUser>
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) :base(options)
+        {
+                
+        }
+    }
+}
