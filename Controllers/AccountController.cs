1f13d3c9496c4a5e6f3246c56a40bb5743bc6d21
OscarwOw <OscarwOw210@gmail.com>
2022-12-14 08:55:10 +0100

    adding Account Controller

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using IdentityAppCourse2022.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityAppCourse2022.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        public AccountController(UserManager<IdentityUser> userManager,SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Register(string returnUrl = null)
+        {
+            RegisterViewModel registerViewModel = new RegisterViewModel();
+            registerViewModel.ReturnUrl = returnUrl;
+            return View(registerViewModel);
+        }
+
+    }
+}
